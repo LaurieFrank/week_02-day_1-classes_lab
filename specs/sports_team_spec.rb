28645a2bffd66a2abc0adbe57c79d521d97afdec
Laurie Frank <laurie360@hotmail.co.uk>
2019-09-16 16:17:39 +0100

    three test complete - section b

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -3,3 +3,20 @@ require("minitest/rg")
 require_relative("../sports_team")
 
 class TestTeam < Minitest::Test
+
+  def test_get_team_name
+    team = Team.new("The Super Confused Team", "12", "Zsolt")
+    assert_equal("The Super Confused Team", team.get_team_name())
+  end
+
+  def test_get_players
+    team = Team.new("The Super Confused Team", "12", "Zsolt")
+    assert_equal("12", team.get_players())
+  end
+
+  def test_get_coach_name
+    team = Team.new("The Super Confused Team", "12", "Zsolt")
+    assert_equal("Zsolt", team.get_coach_name())
+  end
+
+end
--- sports_team.rb
+++ sports_team.rb
@@ -1,1 +1,21 @@
 class Team
+
+  def initialize(input_team_name, input_players, input_coach)
+    @team_name = input_team_name
+    @players = input_players
+    @coach = input_coach
+  end
+
+  def get_team_name
+    return @team_name
+  end
+
+  def get_players
+    return @players
+  end
+
+  def get_coach_name
+    return @coach
+  end
+
+end
