71ca6d4a73fbc5b43bf6125c8f634a2b4eb46873
Laurie Frank <laurie360@hotmail.co.uk>
2019-09-16 16:56:49 +0100

    refactored getter and setter methods

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -6,22 +6,22 @@ class TestTeam < Minitest::Test
 
   def test_get_team_name
     team = Team.new("The Super Confused Team", "12", "Zsolt")
-    assert_equal("The Super Confused Team", team.get_team_name())
+    assert_equal("The Super Confused Team", team.team_name())
   end
 
   def test_get_players
     team = Team.new("The Super Confused Team", "12", "Zsolt")
-    assert_equal("12", team.get_players())
+    assert_equal("12", team.players())
   end
 
   def test_get_coach_name
     team = Team.new("The Super Confused Team", "12", "Zsolt")
-    assert_equal("Zsolt", team.get_coach_name())
+    assert_equal("Zsolt", team.coach())
   end
 
   def test_update_coach_name
     team = Team.new("The Super Confused Team", "12", "Zsolt")
-    team.set_update_coach_name("Harrison")
-    assert_equal("Harrison", team.get_coach_name)
+    team.coach = "Harrison"
+    assert_equal("Harrison", team.coach)
   end
 end
--- sports_team.rb
+++ sports_team.rb
@@ -1,24 +1,26 @@
 class Team
 
+attr_accessor(:team_name, :players, :coach)
+
   def initialize(input_team_name, input_players, input_coach)
     @team_name = input_team_name
     @players = input_players
     @coach = input_coach
   end
 
-  def get_team_name
-    return @team_name
-  end
+  # def get_team_name
+  #   return @team_name
+  # end
 
-  def get_players
-    return @players
-  end
-
-  def get_coach_name
-    return @coach
-  end
-
-  def set_update_coach_name(new_name)
-    @coach = new_name
-  end
+  # def get_players
+  #   return @players
+  # end
+  #
+  # def get_coach_name
+  #   return @coach
+  # end
+  #
+  # def set_update_coach_name(new_name)
+  #   @coach = new_name
+  # end
 end
